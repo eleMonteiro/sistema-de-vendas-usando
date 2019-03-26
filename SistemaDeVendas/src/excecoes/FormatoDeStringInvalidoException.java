2a0644abb3a9f0aed52db1f348b36bccdfcc93ff
Publio <publioblenilio@gmail.com>
2019-03-26 20:05:58 -0300

    Adicionadas as exceções FormatoDeStringInvalidoException e StringVaziaException

---
+++ SistemaDeVendas/src/excecoes/FormatoDeStringInvalidoException.java
@@ -0,0 +1,14 @@
+package excecoes;
+
+public class FormatoDeStringInvalidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4787557193240203199L;
+	
+	public FormatoDeStringInvalidoException(String message) {
+		super(message);
+	}
+	
+}
