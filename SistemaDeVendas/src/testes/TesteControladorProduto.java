4934a525dcfd7a2212f97c327f5a26344a224643
Elenilson <elemonteiro@lu.ufc.br>
2019-03-27 10:18:23 -0300

    Criando a classe de teste

---
+++ SistemaDeVendas/src/testes/TesteControladorProduto.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TesteControladorProduto {
+
+	@Test
+	void TesteControladorProduto() {
+		fail("Not yet implemented");
+	}
+
+}
