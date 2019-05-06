da778e35940c8196af78c16cdc7b11bb23a5a593
eleMonteiro@alu.ufc.br <elemonteiro@alu.ufc.br>
2019-05-06 14:04:38 -0300

    comentários das alterações

--- SistemaDeVendas/src/DAO/IGenericoDAO.java
+++ SistemaDeVendas/src/DAO/IGenericoDAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface IGenericoDAO<T> {
 
 	public long  adicionar(T t);
-    public void editar(T t);
+	public void editar(T t);
     public boolean remover(long id);
     public List<T> buscarPorNome(String nome);
     public T buscarPorId(long id);
